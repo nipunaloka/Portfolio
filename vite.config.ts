2577da0767c7258b3ab4cd61bc2e28307335ddf8
Nipun Bandara <nipunaloka489@gmail.com>
2025-12-22 16:26:51 +0530

    Budg fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
     optimizeDeps: {
